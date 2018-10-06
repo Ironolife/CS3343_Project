c8aa6b560a06df2023f8dd4ebe867de9b6f47f21
IronolifeTest <sampoison@icloud.com>
2018-10-06 13:52:52 +0800

    Commit Test

--- src/ems/User.java
+++ src/ems/User.java
@@ -1,5 +1,19 @@
 package ems;
 
-public class User {
+import java.util.UUID;
+
+public abstract class User {
+	
+	private UUID id;
+	private String name;
+	private int age;
+	private Ticket[] tickets;
+	
+	public User(UUID id, String name, int age) {
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.tickets = new Ticket[] {};
+	}
 
 }
