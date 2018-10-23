77e998dca7002508748fe08daf714bfb8076382c
Rheneas Choi <rheneaschoi@users.noreply.github.com>
2018-10-23 20:25:20 +0800

    Update Coupon.java

--- src/ems/Coupon.java
+++ src/ems/Coupon.java
@@ -1,5 +1,12 @@
 package ems;
 
 public class Coupon {
-
+  public Coupon(double discount, Date expiryDate) {
+    this.discount = discount;
+    this.expiryDate = expiryDate;
+  }
+  
+  public double useCoupon() {
+    return discount;
+  }
 }
