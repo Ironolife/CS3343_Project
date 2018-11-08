348b5f74bbe071e08fac13c14e465a8d39458f50
danhui1998 <43878395+danhui1998@users.noreply.github.com>
2018-11-09 00:08:09 +0800

    VendorTest with testing all methods in Vendor class

--- src/emsTest/VendorTest.java
+++ src/emsTest/VendorTest.java
@@ -11,6 +11,9 @@ import org.junit.Test;
 import ems.BackEnd;
 import ems.Event;
 import ems.Location;
+import ems.Member;
+import ems.Ticket;
+import ems.Transaction;
 import ems.Vendor;
 
 public class VendorTest {
@@ -26,6 +29,11 @@ public class VendorTest {
 	String eventName2;
 	Event event1;
 	Event event2;
+	String userLoginId;
+	String userName;
+	String userPassword;
+	int userAge;
+	String useHKId;
 
 	@Before
 	public void setUp() {
@@ -272,5 +280,331 @@ public class VendorTest {
 		assertEquals(0, tResult, 1);
 		eventList.clear();
 
+	}
+	
+	@Test
+	public void testGetTransactions1() {
+		vendorLoginId = "vendorId";
+		vendorName = "vendorName";
+		vendorPassword = "vendorPassword";
+		newPassword = "newPassword";
+		Vendor tVendor = new Vendor(vendorLoginId, vendorPassword, vendorName);
+		ArrayList<Transaction> transactionList = tVendor.getTransactions();
+		assertEquals(0, transactionList.size());
+	}
+	
+	
+	@Test
+	public void testGetTransactions2() {
+		vendorLoginId = "vendorId";
+		vendorName = "vendorName";
+		vendorPassword = "vendorPassword";
+		newPassword = "newPassword";
+		userLoginId = "human";
+		userName = "human";
+		userPassword = "human123";
+		userAge = 10;
+		useHKId = "A123456";
+		Member purchaser = new Member(userLoginId, userPassword, userName, userAge, useHKId);
+		Vendor tVendor = new Vendor(vendorLoginId, vendorPassword, vendorName);
+		Location location = new Location(locationName, locationCapacity);
+		Event event1 = new Event(eventName1, startTime, endTime, location, true);
+		Ticket aTicket = new Ticket(event1, 12, 100);
+		Transaction aTransaction = new Transaction(aTicket, purchaser, tVendor);
+		tVendor.addTransaction(aTransaction);
+		ArrayList<Transaction> transactionList = tVendor.getTransactions();
+		assertEquals(0, transactionList.size());
+	}
+	
+	@Test
+	public void testGetTransactionsWithVendorHaveTwoTransactionId() {
+		vendorLoginId = "vendorId";
+		vendorName = "vendorName";
+		vendorPassword = "vendorPassword";
+		newPassword = "newPassword";
+		userLoginId = "human";
+		userName = "human";
+		userPassword = "human123";
+		userAge = 10;
+		useHKId = "A123456";
+		Member purchaser = new Member(userLoginId, userPassword, userName, userAge, useHKId);
+		Vendor tVendor = new Vendor(vendorLoginId, vendorPassword, vendorName);
+		Location location = new Location(locationName, locationCapacity);
+		Event event1 = new Event(eventName1, startTime, endTime, location, true);
+		Event event2 = new Event(eventName2, startTime, endTime, location, true);
+		Ticket ticketForEvent1 = new Ticket(event1, 12, 100);
+		Ticket ticketForEvent2 = new Ticket(event2, 20, 18);
+		Transaction transactionForTicket1 = new Transaction(ticketForEvent1, purchaser, tVendor);
+		Transaction transactionForTicket2 = new Transaction(ticketForEvent2, purchaser, tVendor);
+		tVendor.addTransaction(transactionForTicket1);
+		tVendor.addTransaction(transactionForTicket2);
+		ArrayList<Transaction> transactionList = tVendor.getTransactions();
+		assertEquals(0, transactionList.size());
+	}
+	
+	@Test
+	public void testGetTransactionsWithVendorHaveTwoTransactionIdAndBackEndContainsDifferent() {
+		vendorLoginId = "vendorId";
+		vendorName = "vendorName";
+		vendorPassword = "vendorPassword";
+		newPassword = "newPassword";
+		userLoginId = "human";
+		userName = "human";
+		userPassword = "human123";
+		userAge = 10;
+		useHKId = "A123456";
+		Member purchaser = new Member(userLoginId, userPassword, userName, userAge, useHKId);
+		Vendor tVendor = new Vendor(vendorLoginId, vendorPassword, vendorName);
+		Location location = new Location(locationName, locationCapacity);
+		Event event1 = new Event(eventName1, startTime, endTime, location, true);
+		Event event2 = new Event(eventName2, startTime, endTime, location, true);
+		Ticket ticketForEvent1 = new Ticket(event1, 12, 100);
+		Ticket ticketForEvent2 = new Ticket(event2, 20, 18);
+		Transaction transactionForTicket1 = new Transaction(ticketForEvent1, purchaser, tVendor);
+		Transaction transactionForTicket2 = new Transaction(ticketForEvent2, purchaser, tVendor);
+		tVendor.addTransaction(transactionForTicket1);
+		BackEnd backEnd = BackEnd.getInstance();
+		ArrayList<Transaction> backEndTransactionList = backEnd.getTransactions();
+		backEndTransactionList.clear();
+		backEndTransactionList.add(transactionForTicket2);
+		ArrayList<Transaction> transactionList = tVendor.getTransactions();
+		assertEquals(0, transactionList.size());
+		backEndTransactionList.clear();
+	}
+	
+	
+	@Test
+	public void testGetTransactionsWithVendorHaveTwoTransactionIdAndBackEndContainsOneSameTransactionWithAnotherDifferentTransaction() {
+		vendorLoginId = "vendorId";
+		vendorName = "vendorName";
+		vendorPassword = "vendorPassword";
+		newPassword = "newPassword";
+		userLoginId = "human";
+		userName = "human";
+		userPassword = "human123";
+		userAge = 10;
+		useHKId = "A123456";
+		Member purchaser = new Member(userLoginId, userPassword, userName, userAge, useHKId);
+		Vendor tVendor = new Vendor(vendorLoginId, vendorPassword, vendorName);
+		Location location = new Location(locationName, locationCapacity);
+		Event event1 = new Event(eventName1, startTime, endTime, location, true);
+		Event event2 = new Event(eventName2, startTime, endTime, location, true);
+		Ticket ticketForEvent1 = new Ticket(event1, 12, 100);
+		Ticket ticketForEvent2 = new Ticket(event2, 20, 18);
+		Transaction transactionForTicket1 = new Transaction(ticketForEvent1, purchaser, tVendor);
+		Transaction transactionForTicket2 = new Transaction(ticketForEvent2, purchaser, tVendor);
+		tVendor.addTransaction(transactionForTicket2);
+		BackEnd backEnd = BackEnd.getInstance();
+		ArrayList<Transaction> backEndTransactionList = backEnd.getTransactions();
+		backEndTransactionList.clear();
+		backEndTransactionList.add(transactionForTicket1);
+		backEndTransactionList.add(transactionForTicket2);
+		ArrayList<Transaction> transactionList = tVendor.getTransactions();
+		assertEquals(1, transactionList.size());
+		backEndTransactionList.clear();
+	}
+	
+	@Test
+	public void testRemoveTransactionWithNoTransanctionInVendor() {
+		vendorLoginId = "vendorId";
+		vendorName = "vendorName";
+		vendorPassword = "vendorPassword";
+		newPassword = "newPassword";
+		userLoginId = "human";
+		userName = "human";
+		userPassword = "human123";
+		userAge = 10;
+		useHKId = "A123456";
+		Member purchaser = new Member(userLoginId, userPassword, userName, userAge, useHKId);
+		Vendor tVendor = new Vendor(vendorLoginId, vendorPassword, vendorName);
+		Location location = new Location(locationName, locationCapacity);
+		Event event1 = new Event(eventName1, startTime, endTime, location, true);
+		Ticket ticketForEvent1 = new Ticket(event1, 12, 100);
+		Transaction transactionForTicket1 = new Transaction(ticketForEvent1, purchaser, tVendor);
+		Transaction tResult = tVendor.removeTransaction(transactionForTicket1);
+		assertEquals(null, tResult);
+	}
+	
+	@Test
+	public void testRemoveTransactionWithOneTransanctionInVendor() {
+		vendorLoginId = "vendorId";
+		vendorName = "vendorName";
+		vendorPassword = "vendorPassword";
+		newPassword = "newPassword";
+		userLoginId = "human";
+		userName = "human";
+		userPassword = "human123";
+		userAge = 10;
+		useHKId = "A123456";
+		Member purchaser = new Member(userLoginId, userPassword, userName, userAge, useHKId);
+		Vendor tVendor = new Vendor(vendorLoginId, vendorPassword, vendorName);
+		Location location = new Location(locationName, locationCapacity);
+		Event event1 = new Event(eventName1, startTime, endTime, location, true);
+		Ticket ticketForEvent1 = new Ticket(event1, 12, 100);
+		Transaction transactionForTicket1 = new Transaction(ticketForEvent1, purchaser, tVendor);
+		tVendor.addTransaction(transactionForTicket1);
+		Transaction tResult = tVendor.removeTransaction(transactionForTicket1);
+		assertEquals(transactionForTicket1, tResult);
+	}
+	
+	@Test
+	public void testgetAccumulatedSales1() {
+		vendorLoginId = "vendorId";
+		vendorName = "vendorName";
+		vendorPassword = "vendorPassword";
+		newPassword = "newPassword";
+		userLoginId = "human";
+		userName = "human";
+		userPassword = "human123";
+		userAge = 10;
+		useHKId = "A123456";
+		Member purchaser = new Member(userLoginId, userPassword, userName, userAge, useHKId);
+		Vendor tVendor = new Vendor(vendorLoginId, vendorPassword, vendorName);
+		Location location = new Location(locationName, locationCapacity);
+		Event event1 = new Event(eventName1, startTime, endTime, location, true);
+		Ticket ticketForEvent1 = new Ticket(event1, 12, 100);
+		Transaction transactionForTicket1 = new Transaction(ticketForEvent1, purchaser, tVendor);
+		double tResult = tVendor.getAccumulatedSales();
+		assertEquals(0, tResult, 1);
+	}
+	
+	@Test
+	public void testgetAccumulatedSales2() {
+		vendorLoginId = "vendorId";
+		vendorName = "vendorName";
+		vendorPassword = "vendorPassword";
+		newPassword = "newPassword";
+		userLoginId = "human";
+		userName = "human";
+		userPassword = "human123";
+		userAge = 10;
+		useHKId = "A123456";
+		Member purchaser = new Member(userLoginId, userPassword, userName, userAge, useHKId);
+		Vendor tVendor = new Vendor(vendorLoginId, vendorPassword, vendorName);
+		Location location = new Location(locationName, locationCapacity);
+		Event event1 = new Event(eventName1, startTime, endTime, location, true);
+		Ticket ticketForEvent1 = new Ticket(event1, 12, 100);
+		Transaction transactionForTicket1 = new Transaction(ticketForEvent1, purchaser, tVendor);
+		BackEnd backEnd = BackEnd.getInstance();
+		ArrayList<Transaction> backEndTransactionList = backEnd.getTransactions();
+		backEndTransactionList.clear();
+		backEndTransactionList.add(transactionForTicket1);
+		tVendor.addTransaction(transactionForTicket1);
+		double tResult = tVendor.getAccumulatedSales();
+		assertEquals(12, tResult, 1);
+		backEndTransactionList.clear();
+	}
+	
+	@Test
+	public void testgetAccumulatedSales3() {
+		vendorLoginId = "vendorId";
+		vendorName = "vendorName";
+		vendorPassword = "vendorPassword";
+		newPassword = "newPassword";
+		userLoginId = "human";
+		userName = "human";
+		userPassword = "human123";
+		userAge = 10;
+		useHKId = "A123456";
+		Member purchaser = new Member(userLoginId, userPassword, userName, userAge, useHKId);
+		Vendor tVendor = new Vendor(vendorLoginId, vendorPassword, vendorName);
+		Location location = new Location(locationName, locationCapacity);
+		Event event1 = new Event(eventName1, startTime, endTime, location, true);
+		Event event2 = new Event(eventName2, startTime, endTime, location, true);
+		Ticket ticketForEvent1 = new Ticket(event1, 12, 100);
+		Ticket ticketForEvent2 = new Ticket(event2, 20, 18);
+		Transaction transactionForTicket1 = new Transaction(ticketForEvent1, purchaser, tVendor);
+		Transaction transactionForTicket2 = new Transaction(ticketForEvent2, purchaser, tVendor);
+		BackEnd backEnd = BackEnd.getInstance();
+		ArrayList<Transaction> backEndTransactionList = backEnd.getTransactions();
+		backEndTransactionList.clear();
+		backEndTransactionList.add(transactionForTicket1);
+		backEndTransactionList.add(transactionForTicket2);
+		tVendor.addTransaction(transactionForTicket1);
+		tVendor.addTransaction(transactionForTicket2);
+		double tResult = tVendor.getAccumulatedSales();
+		assertEquals(32, tResult, 1);
+		backEndTransactionList.clear();
+	}
+	
+	@Test
+	public void testgetAccumulatedProfit1() {
+		vendorLoginId = "vendorId";
+		vendorName = "vendorName";
+		vendorPassword = "vendorPassword";
+		newPassword = "newPassword";
+		userLoginId = "human";
+		userName = "human";
+		userPassword = "human123";
+		userAge = 10;
+		useHKId = "A123456";
+		Member purchaser = new Member(userLoginId, userPassword, userName, userAge, useHKId);
+		Vendor tVendor = new Vendor(vendorLoginId, vendorPassword, vendorName);
+		Location location = new Location(locationName, locationCapacity);
+		Event event1 = new Event(eventName1, startTime, endTime, location, true);
+		Ticket ticketForEvent1 = new Ticket(event1, 12, 100);
+		double tResult = tVendor.getAccumulatedProfit();
+		assertEquals(0, tResult, 1);
+	}
+	
+	@Test
+	public void testgetAccumulatedProfit2() {
+		vendorLoginId = "vendorId";
+		vendorName = "vendorName";
+		vendorPassword = "vendorPassword";
+		newPassword = "newPassword";
+		userLoginId = "human";
+		userName = "human";
+		userPassword = "human123";
+		userAge = 10;
+		useHKId = "A123456";
+		Member purchaser = new Member(userLoginId, userPassword, userName, userAge, useHKId);
+		Vendor tVendor = new Vendor(vendorLoginId, vendorPassword, vendorName);
+		Location location = new Location(locationName, locationCapacity);
+		Event event1 = new Event(eventName1, startTime, endTime, location, true);
+		Ticket ticketForEvent1 = new Ticket(event1, 12, 100);
+		Transaction transactionForTicket1 = new Transaction(ticketForEvent1, purchaser, tVendor);
+		BackEnd backEnd = BackEnd.getInstance();
+		ArrayList<Transaction> backEndTransactionList = backEnd.getTransactions();
+		backEndTransactionList.clear();
+		backEndTransactionList.add(transactionForTicket1);
+		tVendor.addTransaction(transactionForTicket1);
+		double tResult = tVendor.getAccumulatedProfit();
+		double expectedResult = ticketForEvent1.getPrice()*purchaser.getDiscount();
+		assertEquals(expectedResult, tResult, 2);
+		backEndTransactionList.clear();
+	}
+	
+	@Test
+	public void testgetAccumulatedProfit3() {
+		vendorLoginId = "vendorId";
+		vendorName = "vendorName";
+		vendorPassword = "vendorPassword";
+		newPassword = "newPassword";
+		userLoginId = "human";
+		userName = "human";
+		userPassword = "human123";
+		userAge = 10;
+		useHKId = "A123456";
+		Member purchaser = new Member(userLoginId, userPassword, userName, userAge, useHKId);
+		Vendor tVendor = new Vendor(vendorLoginId, vendorPassword, vendorName);
+		Location location = new Location(locationName, locationCapacity);
+		Event event1 = new Event(eventName1, startTime, endTime, location, true);
+		Event event2 = new Event(eventName2, startTime, endTime, location, true);
+		Ticket ticketForEvent1 = new Ticket(event1, 12, 100);
+		Ticket ticketForEvent2 = new Ticket(event2, 20, 18);
+		Transaction transactionForTicket1 = new Transaction(ticketForEvent1, purchaser, tVendor);
+		Transaction transactionForTicket2 = new Transaction(ticketForEvent2, purchaser, tVendor);
+		BackEnd backEnd = BackEnd.getInstance();
+		ArrayList<Transaction> backEndTransactionList = backEnd.getTransactions();
+		backEndTransactionList.clear();
+		backEndTransactionList.add(transactionForTicket1);
+		backEndTransactionList.add(transactionForTicket2);
+		tVendor.addTransaction(transactionForTicket1);
+		tVendor.addTransaction(transactionForTicket2);
+		double tResult = tVendor.getAccumulatedProfit();
+		double expectResult = ticketForEvent1.getPrice()*purchaser.getDiscount() + ticketForEvent2.getPrice()*purchaser.getDiscount();
+		assertEquals(expectResult, tResult, 1);
+		backEndTransactionList.clear();
 	}
 }
