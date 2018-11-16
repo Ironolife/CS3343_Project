f9be16768413c65783d74904abe6e56c218eed02
Rheneas Choi <rheneaschoi@hotmail.com>
2018-11-17 02:06:25 +0800

    Create new test case file - BackEndTest

---
+++ src/emsTest/BackEndTest.java
@@ -0,0 +1,154 @@
+package emsTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BackEndTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetInstance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSerialize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateNewVendor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetVendors() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveVendor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUsers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateNewLocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLocations() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveLocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsDuplicateLocationName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateNewEvent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEvents() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveEvent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateNewTickets() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTickets() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveTicket() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateNewCoupon() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCoupons() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveCoupon() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsDuplicateCouponCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateNewReview() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetReviews() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveReview() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateNewTransaction() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTransactions() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveTransaction() {
+		fail("Not yet implemented");
+	}
+
+}
