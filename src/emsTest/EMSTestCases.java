cfe3f5c99ee432c880c92654ae099515c84e5a56
danhui1998 <43878395+danhui1998@users.noreply.github.com>
2018-11-28 02:05:11 +0800

    EMSTestCases with more test cases

--- src/emsTest/EMSTestCases.java
+++ src/emsTest/EMSTestCases.java
@@ -223,7 +223,7 @@ public class EMSTestCases {
 		System.out.println();
 		System.out.println("LoginId: ");
 		System.out.println("Password: ");
-		EMS.PrintHeader("- Welcome " + ((Member)this.userWeWillUse).getName() + " -");
+		EMS.PrintHeader("- Welcome " + ((Member) this.userWeWillUse).getName() + " -");
 		System.out.println("Choose operations: ");
 		System.out.println("1: Display Events");
 		System.out.println("2: Search Events");
@@ -267,10 +267,8 @@ public class EMSTestCases {
 				} else if (this.readInputCount == 4) {
 					this.readInputCount++;
 					return "U1";
-				} 
-					return "U1";
-				
-			
+				}
+				return "U1";
 
 			}
 		}
@@ -304,7 +302,7 @@ public class EMSTestCases {
 		System.out.println();
 		System.out.println("LoginId: ");
 		System.out.println("Password: ");
-		EMS.PrintHeader("- Welcome " + ((Member)user1).getName() + " -");
+		EMS.PrintHeader("- Welcome " + ((Member) user1).getName() + " -");
 		System.out.println("Choose operations: ");
 		System.out.println("1: Display Events");
 		System.out.println("2: Search Events");
@@ -378,13 +376,13 @@ public class EMSTestCases {
 				} else if (this.readInputCount == 14) {
 					this.readInputCount++;
 					return "20";
-				} else if(this.readInputCount == 15) {
+				} else if (this.readInputCount == 15) {
 					this.readInputCount++;
 					return "3";
-				} else if(this.readInputCount == 16) {
+				} else if (this.readInputCount == 16) {
 					this.readInputCount++;
 					return "2";
-				}  else if(this.readInputCount == 17) {
+				} else if (this.readInputCount == 17) {
 					this.readInputCount++;
 					return "U4";
 				} else if (this.readInputCount == 18) {
@@ -398,7 +396,6 @@ public class EMSTestCases {
 					return "U1";
 				}
 				return "8";
-				
 
 			}
 		}
@@ -466,7 +463,237 @@ public class EMSTestCases {
 		EMS.PrintHeader("- Login -");
 		System.out.println("LoginId: ");
 		System.out.println("Password: ");
-		EMS.PrintHeader("- Welcome " + ((Member)user1).getName() + " -");
+		EMS.PrintHeader("- Welcome " + ((Member) user1).getName() + " -");
+		System.out.println("Choose operations: ");
+		System.out.println("1: Display Events");
+		System.out.println("2: Search Events");
+		System.out.println("3: My Upcoming Events");
+		System.out.println("4: Purchase Ticket");
+		System.out.println("5: Review Event");
+		System.out.println("6: My Account");
+		System.out.println("7: Purchase History");
+		System.out.println("8: Logout");
+		System.out.println();
+		ByteArrayOutputStream outContent2 = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent2));
+		EMSStub tEMSStub = new EMSStub();
+		assertEquals(outContent1.toString(), outContent2.toString());
+	}
+
+	@Test
+	public void testGuestRegister() {
+
+		class EMSStub extends EMS {
+			int readInputCount = 0;
+
+			public EMSStub() {
+				super();
+			}
+
+			@Override
+			public String readInput() {
+				if (this.readInputCount == 0) {
+					this.readInputCount++;
+					return "2";
+				} else if (this.readInputCount == 1) {
+					this.readInputCount++;
+					return "1"; //
+				} else if (this.readInputCount == 2) {
+					this.readInputCount++;
+					return "U1";
+				} else if (this.readInputCount == 3) {
+					this.readInputCount++;
+					return "U2";
+				} else if (this.readInputCount == 4) {
+					this.readInputCount++;
+					return "U4";
+				} else if (this.readInputCount == 5) {
+					this.readInputCount++;
+					return "U4";
+				} else if (this.readInputCount == 6) {
+					this.readInputCount++;
+					return "A1234567";
+				} else if (this.readInputCount == 7) {
+					this.readInputCount++;
+					return "A1234567";
+				} else if (this.readInputCount == 8) {
+					this.readInputCount++;
+					return "B1234567";
+				} else if (this.readInputCount == 9) {
+					this.readInputCount++;
+					return "-50";
+				} else if (this.readInputCount == 10) {
+					this.readInputCount++;
+					return "188";
+				} else if (this.readInputCount == 11) {
+					this.readInputCount++;
+					return "ABCDE";
+				} else if (this.readInputCount == 12) {
+					this.readInputCount++;
+					return "20";
+				} else if (this.readInputCount == 13) {
+					this.readInputCount++;
+					return "1";//
+				} else if (this.readInputCount == 14) {
+					this.readInputCount++;
+					return "1";
+				} else if (this.readInputCount == 15) {
+					this.readInputCount++;
+					return "U1";
+				} else if (this.readInputCount == 16) {
+					this.readInputCount++;
+					return "U1";
+				}
+				return "8";
+
+			}
+		}
+		user1 = new Member("U1", "U1", "U1", 20, "A1234567");
+		user2 = new Member("U2", "U2", "U2", 20, "A1234567");
+		userWeWillUse = new Member("U3", "U3", "U3", 20, "A1234567");
+		backEndVendorList = backEnd.getVendors();
+		backEndVendorList.clear();
+		this.backEndUserList = backEnd.getUsers();
+		this.backEndUserList.clear();
+		backEndUserList.add(user1);
+		backEndUserList.add(user2);
+		backEndUserList.add(userWeWillUse);
+		String input = "8";
+		System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+		ByteArrayOutputStream outContent1 = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent1));
+		EMS.PrintHeader("- Event Management System -");
+		System.out.println("Choose operations: ");
+		System.out.println("1: Login");
+		System.out.println("2: Register");
+		EMS.PrintHeader("- Register -");
+		System.out.println("Account Type: ");
+		System.out.println("1: User");
+		System.out.println("2: Vendor");
+
+		System.out.println("LoginId: ");
+		EMS.PrintHeader("Duplicate LoginId! Please try another one.");
+		System.out.println("LoginId: ");
+		EMS.PrintHeader("Duplicate LoginId! Please try another one.");
+		System.out.println("LoginId: ");
+		System.out.println("Password: ");
+		System.out.println("HKID: ");
+		EMS.PrintHeader("HKID already registered!");
+		System.out.println("HKID: ");
+		EMS.PrintHeader("HKID already registered!");
+		System.out.println("HKID: ");
+		System.out.println("Age: ");
+		EMS.PrintHeader("Invalid Input!");
+		System.out.println("Age: ");
+		EMS.PrintHeader("Invalid Input!");
+		System.out.println("Age: ");
+		EMS.PrintHeader("Invalid Input!");
+		System.out.println("Age: ");
+		System.out.println("Please select an account type: ");
+		System.out.println("1: Guest");
+		System.out.println("2: Member");
+
+		
+		EMS.PrintHeader("Guest Created!");
+		System.out.println("Choose operations: ");
+		System.out.println("1: Login");
+		System.out.println("2: Register");
+		EMS.PrintHeader("- Login -");
+		System.out.println("LoginId: ");
+		System.out.println("Password: ");
+		EMS.PrintHeader("- Welcome " + ((Member) user1).getName() + " -");
+		System.out.println("Choose operations: ");
+		System.out.println("1: Display Events");
+		System.out.println("2: Search Events");
+		System.out.println("3: My Upcoming Events");
+		System.out.println("4: Purchase Ticket");
+		System.out.println("5: Review Event");
+		System.out.println("6: My Account");
+		System.out.println("7: Purchase History");
+		System.out.println("8: Logout");
+		System.out.println();
+		ByteArrayOutputStream outContent2 = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent2));
+		EMSStub tEMSStub = new EMSStub();
+		assertEquals(outContent1.toString(), outContent2.toString());
+	}
+	
+	@Test
+	public void testVendorRegister() {
+
+		class EMSStub extends EMS {
+			int readInputCount = 0;
+
+			public EMSStub() {
+				super();
+			}
+
+			@Override
+			public String readInput() {
+				if (this.readInputCount == 0) {
+					this.readInputCount++;
+					return "2";
+				} else if (this.readInputCount == 1) {
+					this.readInputCount++;
+					return "2"; //
+				} else if (this.readInputCount == 2) {
+					this.readInputCount++;
+					return "V1";
+				} else if (this.readInputCount == 3) {
+					this.readInputCount++;
+					return "V1";
+				} else if (this.readInputCount == 4) {
+					this.readInputCount++;
+					return "V1";
+				}  else if (this.readInputCount == 5) {
+					this.readInputCount++;
+					return "1";
+				} else if (this.readInputCount == 6) {
+					this.readInputCount++;
+					return "U1";
+				} else if (this.readInputCount == 7) {
+					this.readInputCount++;
+					return "U1";
+				}
+				return "8";
+
+			}
+		}
+		user1 = new Member("U1", "U1", "U1", 20, "A1234567");
+		user2 = new Member("U2", "U2", "U2", 20, "A1234567");
+		userWeWillUse = new Member("U3", "U3", "U3", 20, "A1234567");
+		backEndVendorList = backEnd.getVendors();
+		backEndVendorList.clear();
+		this.backEndUserList = backEnd.getUsers();
+		this.backEndUserList.clear();
+		backEndUserList.add(user1);
+		backEndUserList.add(user2);
+		backEndUserList.add(userWeWillUse);
+		String input = "8";
+		System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+		ByteArrayOutputStream outContent1 = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent1));
+		EMS.PrintHeader("- Event Management System -");
+		System.out.println("Choose operations: ");
+		System.out.println("1: Login");
+		System.out.println("2: Register");
+		EMS.PrintHeader("- Register -");
+		System.out.println("Account Type: ");
+		System.out.println("1: User");
+		System.out.println("2: Vendor");
+		System.out.println("LoginId: ");
+		System.out.println("Password: ");
+		System.out.println("Vendor Name: ");
+		EMS.PrintHeader("Vendor Created!");
+		System.out.println("Choose operations: ");
+		System.out.println("1: Login");
+		System.out.println("2: Register");
+		EMS.PrintHeader("- Login -");
+		System.out.println("LoginId: ");
+		System.out.println("Password: ");
+		EMS.PrintHeader("- Welcome " + ((Member) user1).getName() + " -");
 		System.out.println("Choose operations: ");
 		System.out.println("1: Display Events");
 		System.out.println("2: Search Events");
